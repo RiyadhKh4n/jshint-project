5bb90bc6f22b70678d200ebf40535e489bcc381b
Arkay <riyadh.k@outlook.com>
2022-02-08 16:07:25 +0000

    Link api key

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "IfGGBN0h4c_wvA3sxM642UNwpdE";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultModla = new bootstrap.Modal(document.getElementById("resultsModal"));
